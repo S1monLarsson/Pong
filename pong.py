fadbcf5437b96e2e3db991c795bcc43acf93d289
S1monLarsson <simonlar@student.chalmers.se>
2021-09-30 17:46:52 +0200

    start implementing a computer player

--- pong.py
+++ pong.py
@@ -15,13 +15,23 @@ paddle_l = Paddle("Player A", -350)
 # Right paddle
 paddle_r = Paddle("Player B", 350)
 
+# 1 or 2 players
+computer = True
+
 # Ball
 ball = turtle.Turtle(shape="circle")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
-ball.dx = 0.15
-ball.dy = 0.15
+ball.default_speed = 0.15
+ball.dx = ball.default_speed
+ball.dy = ball.default_speed
+ball.increase = 0.01
+loops = 0
+
+# Ball position to make computer player
+old_ball_x = 0
+old_ball_y = 0
 
 # Middle line
 line = turtle.Turtle(shape="square")
@@ -39,12 +49,17 @@ score.goto(0, 250)
 score.write("{}            {}".format(paddle_l.score, paddle_r.score), 
           align="center", font=("Courier", 24, "normal"))
 
-# Keyboard binding, paddle moves on release
+# Keyboard binding, paddle moves on release when 2 players
+# Otherwise on push down
 wn.listen()
-wn.onkey(paddle_l.up, "w")
-wn.onkey(paddle_l.down, "s")
-wn.onkey(paddle_r.up, "Up")
-wn.onkey(paddle_r.down, "Down")
+if not computer:
+    wn.onkey(paddle_l.up, "w")
+    wn.onkey(paddle_l.down, "s")
+    wn.onkey(paddle_r.up, "Up")
+    wn.onkey(paddle_r.down, "Down")
+else:
+    wn.onkeypress(paddle_r.up, "Up")
+    wn.onkeypress(paddle_r.down, "Down")
 wn.onkey(wn.bye, "q")
 
 
@@ -70,6 +85,9 @@ def goal(paddle):
     paddle.score+=1
     ball.goto(0,0)
     ball.dx *= -1
+    ball.dx = ball.default_speed
+    ball.dy = ball.default_speed
+    ball.default_speed
     if paddle.score == 10:
         score.clear()
         score.write("{} won!  ".format(paddle.name), 
@@ -77,15 +95,37 @@ def goal(paddle):
         wn.mainloop()
     else:
         update_score()
+
+# Computer actions
+def move_c_paddle(paddle_y, ball_y):
+    if paddle_y+20 < ball_y:
+        paddle_l.up()
+    elif paddle_y-20 > ball_y: paddle_l.down()
         
 
 # Game loop
 while True:
     wn.update()
 
+
+    # Set old ball coordinates
+    old_ball_x = ball.xcor()
+    old_ball_y = ball.ycor()
+
+    # Increase speed of ball
+    if loops == 600: # Change this to time
+        print(ball.dx)
+        ball.dx += ball.increase * ball.dx
+        ball.dy += ball.increase * ball.dy
+        loops = 0
+    
+
     # Move ball
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
+
+    # Move computer player
+    move_c_paddle(paddle_l.get_y(), ball.ycor())
 
     # Border checking
     if ball.ycor() > 290:
@@ -108,6 +148,8 @@ while True:
         if not paddle_bounce(paddle_l, -330):
             goal(paddle_r)
 
+
+    loops += 1
         
 
 
